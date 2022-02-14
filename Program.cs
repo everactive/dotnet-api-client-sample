c57323d8542d1951f51460674fdb90f78b856b33
Rob Cook <robert.cook@everactive.com>
2022-02-14 11:10:38 -0500

    update authentication for new endpoint and audience value

--- Program.cs
+++ Program.cs
@@ -11,20 +11,24 @@ namespace EveractiveApiDemo
     {
         static void Main(string[] args)
         {
+            var audience = Environment.GetEnvironmentVariable("EVERACTIVE_AUDIENCE");
+            var authUrl = Environment.GetEnvironmentVariable("EVERACTIVE_AUTH_URL");
             var baseUrl = Environment.GetEnvironmentVariable("EVERACTIVE_API_URL");
             string clientId = Environment.GetEnvironmentVariable("EVERACTIVE_CLIENT_ID");
             string clientSecret = Environment.GetEnvironmentVariable("EVERACTIVE_CLIENT_SECRET");
 
-            if (string.IsNullOrEmpty(baseUrl) ||
-                string.IsNullOrEmpty(clientId) || 
+            if (string.IsNullOrEmpty(audience) ||
+                string.IsNullOrEmpty(authUrl) ||
+                string.IsNullOrEmpty(baseUrl) ||
+                string.IsNullOrEmpty(clientId) ||
                 string.IsNullOrEmpty(clientSecret)) 
             {
-                Console.WriteLine("EVERACTIVE_API_URL, EVERACTIVE_CLIENT_ID and EVERACTIVE_CLIENT_SECRET must be defined");
+                Console.WriteLine("EVERACTIVE_AUDIENCE, EVERACTIVE_AUTH_URL, EVERACTIVE_API_URL, EVERACTIVE_CLIENT_ID and EVERACTIVE_CLIENT_SECRET must be defined");
                 Console.ReadLine();
                 return;
             }
 
-            var apiService = new ApiService(baseUrl, clientId, clientSecret);
+            var apiService = new ApiService(audience, authUrl, baseUrl, clientId, clientSecret);
             var traps = apiService.GetSteamTraps().Result;
 
             Console.WriteLine($"Returned {traps.Count} steam traps");
@@ -36,15 +40,22 @@ namespace EveractiveApiDemo
     {
         private static readonly HttpClient _httpClient = new HttpClient();
 
+
         private AuthToken _authToken = new AuthToken();
+        private string _audience;
+        private string _authUrl;
         private string _clientId;
         private string _clientSecret;
 
         public ApiService(
+            string audience,
+            string authUrl,
             string baseAddress,
             string clientId,
             string clientSecret)
         {
+            _audience = audience;
+            _authUrl = authUrl;
             _clientId = clientId;
             _clientSecret = clientSecret;
 
@@ -69,12 +80,13 @@ namespace EveractiveApiDemo
             if (this._authToken.Expires <= DateTime.UtcNow)
             {
                 var dict = new Dictionary<string, string>();
+                dict.Add("audience", _audience);
                 dict.Add("client_id", _clientId);
                 dict.Add("client_secret", _clientSecret);
                 dict.Add("grant_type", "client_credentials");
                 var content = new FormUrlEncodedContent(dict);
 
-                var response = await _httpClient.PostAsync("v2020-07/auth/token/", content);
+                var response = await _httpClient.PostAsync(_authUrl, content);
                 if (!response.IsSuccessStatusCode)
                 {
                     Console.WriteLine($"Unsuccessful authentication - {response.StatusCode}");
